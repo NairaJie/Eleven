bef2909d57559e66b1083b9bf86532a7734ff1c4
NairaJie <Nairaxaviera@gmail.com>
2024-07-24 09:44:56 +0700

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,317 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtid = New System.Windows.Forms.TextBox()
+        Me.txtlama = New System.Windows.Forms.TextBox()
+        Me.txtnama = New System.Windows.Forms.TextBox()
+        Me.rbmember = New System.Windows.Forms.RadioButton()
+        Me.rbnon = New System.Windows.Forms.RadioButton()
+        Me.cmbjenis = New System.Windows.Forms.ComboBox()
+        Me.dttanggal = New System.Windows.Forms.DateTimePicker()
+        Me.cbbuku = New System.Windows.Forms.CheckBox()
+        Me.cbpensil = New System.Windows.Forms.CheckBox()
+        Me.cbpena = New System.Windows.Forms.CheckBox()
+        Me.bttambah = New System.Windows.Forms.Button()
+        Me.btkeluar = New System.Windows.Forms.Button()
+        Me.bthapus = New System.Windows.Forms.Button()
+        Me.btsimpan = New System.Windows.Forms.Button()
+        Me.dgview = New System.Windows.Forms.DataGridView()
+        CType(Me.dgview, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(681, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(211, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Data Penyewaan Buku"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(24, 143)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "ID Sewa"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(24, 206)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(150, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Nama Penyewa"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(24, 262)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(137, 25)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Tipe Penyewa"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(24, 321)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(109, 25)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Jenis Buku"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(24, 387)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(148, 25)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Tanggal Pinjam"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(24, 460)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(125, 25)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Lama Pinjam"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(24, 537)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(68, 25)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Bonus"
+        '
+        'txtid
+        '
+        Me.txtid.Location = New System.Drawing.Point(179, 139)
+        Me.txtid.Name = "txtid"
+        Me.txtid.Size = New System.Drawing.Size(239, 29)
+        Me.txtid.TabIndex = 8
+        '
+        'txtlama
+        '
+        Me.txtlama.Location = New System.Drawing.Point(179, 460)
+        Me.txtlama.Name = "txtlama"
+        Me.txtlama.Size = New System.Drawing.Size(239, 29)
+        Me.txtlama.TabIndex = 9
+        '
+        'txtnama
+        '
+        Me.txtnama.Location = New System.Drawing.Point(179, 202)
+        Me.txtnama.Name = "txtnama"
+        Me.txtnama.Size = New System.Drawing.Size(239, 29)
+        Me.txtnama.TabIndex = 10
+        '
+        'rbmember
+        '
+        Me.rbmember.AutoSize = True
+        Me.rbmember.Location = New System.Drawing.Point(179, 262)
+        Me.rbmember.Name = "rbmember"
+        Me.rbmember.Size = New System.Drawing.Size(109, 29)
+        Me.rbmember.TabIndex = 11
+        Me.rbmember.TabStop = True
+        Me.rbmember.Text = "Member"
+        Me.rbmember.UseVisualStyleBackColor = True
+        '
+        'rbnon
+        '
+        Me.rbnon.AutoSize = True
+        Me.rbnon.Location = New System.Drawing.Point(409, 262)
+        Me.rbnon.Name = "rbnon"
+        Me.rbnon.Size = New System.Drawing.Size(150, 29)
+        Me.rbnon.TabIndex = 12
+        Me.rbnon.TabStop = True
+        Me.rbnon.Text = "Non Member"
+        Me.rbnon.UseVisualStyleBackColor = True
+        '
+        'cmbjenis
+        '
+        Me.cmbjenis.FormattingEnabled = True
+        Me.cmbjenis.Items.AddRange(New Object() {"Novel", "Majalah", "Komik"})
+        Me.cmbjenis.Location = New System.Drawing.Point(179, 314)
+        Me.cmbjenis.Name = "cmbjenis"
+        Me.cmbjenis.Size = New System.Drawing.Size(239, 32)
+        Me.cmbjenis.TabIndex = 13
+        '
+        'dttanggal
+        '
+        Me.dttanggal.Location = New System.Drawing.Point(179, 383)
+        Me.dttanggal.Name = "dttanggal"
+        Me.dttanggal.Size = New System.Drawing.Size(317, 29)
+        Me.dttanggal.TabIndex = 14
+        '
+        'cbbuku
+        '
+        Me.cbbuku.AutoSize = True
+        Me.cbbuku.Location = New System.Drawing.Point(179, 533)
+        Me.cbbuku.Name = "cbbuku"
+        Me.cbbuku.Size = New System.Drawing.Size(83, 29)
+        Me.cbbuku.TabIndex = 15
+        Me.cbbuku.Text = "Buku"
+        Me.cbbuku.UseVisualStyleBackColor = True
+        '
+        'cbpensil
+        '
+        Me.cbpensil.AutoSize = True
+        Me.cbpensil.Location = New System.Drawing.Point(575, 533)
+        Me.cbpensil.Name = "cbpensil"
+        Me.cbpensil.Size = New System.Drawing.Size(91, 29)
+        Me.cbpensil.TabIndex = 16
+        Me.cbpensil.Text = "Pensil"
+        Me.cbpensil.UseVisualStyleBackColor = True
+        '
+        'cbpena
+        '
+        Me.cbpena.AutoSize = True
+        Me.cbpena.Location = New System.Drawing.Point(375, 533)
+        Me.cbpena.Name = "cbpena"
+        Me.cbpena.Size = New System.Drawing.Size(84, 29)
+        Me.cbpena.TabIndex = 17
+        Me.cbpena.Text = "Pena"
+        Me.cbpena.UseVisualStyleBackColor = True
+        '
+        'bttambah
+        '
+        Me.bttambah.Location = New System.Drawing.Point(37, 615)
+        Me.bttambah.Name = "bttambah"
+        Me.bttambah.Size = New System.Drawing.Size(94, 39)
+        Me.bttambah.TabIndex = 18
+        Me.bttambah.Text = "Tambah"
+        Me.bttambah.UseVisualStyleBackColor = True
+        '
+        'btkeluar
+        '
+        Me.btkeluar.Location = New System.Drawing.Point(469, 615)
+        Me.btkeluar.Name = "btkeluar"
+        Me.btkeluar.Size = New System.Drawing.Size(94, 39)
+        Me.btkeluar.TabIndex = 19
+        Me.btkeluar.Text = "Keluar"
+        Me.btkeluar.UseVisualStyleBackColor = True
+        '
+        'bthapus
+        '
+        Me.bthapus.Location = New System.Drawing.Point(324, 615)
+        Me.bthapus.Name = "bthapus"
+        Me.bthapus.Size = New System.Drawing.Size(94, 39)
+        Me.bthapus.TabIndex = 20
+        Me.bthapus.Text = "Hapus"
+        Me.bthapus.UseVisualStyleBackColor = True
+        '
+        'btsimpan
+        '
+        Me.btsimpan.Location = New System.Drawing.Point(179, 615)
+        Me.btsimpan.Name = "btsimpan"
+        Me.btsimpan.Size = New System.Drawing.Size(94, 39)
+        Me.btsimpan.TabIndex = 21
+        Me.btsimpan.Text = "Simpan"
+        Me.btsimpan.UseVisualStyleBackColor = True
+        '
+        'dgview
+        '
+        Me.dgview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgview.Location = New System.Drawing.Point(12, 709)
+        Me.dgview.Name = "dgview"
+        Me.dgview.RowHeadersWidth = 72
+        Me.dgview.RowTemplate.Height = 31
+        Me.dgview.Size = New System.Drawing.Size(779, 372)
+        Me.dgview.TabIndex = 22
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1702, 907)
+        Me.Controls.Add(Me.dgview)
+        Me.Controls.Add(Me.btsimpan)
+        Me.Controls.Add(Me.bthapus)
+        Me.Controls.Add(Me.btkeluar)
+        Me.Controls.Add(Me.bttambah)
+        Me.Controls.Add(Me.cbpena)
+        Me.Controls.Add(Me.cbpensil)
+        Me.Controls.Add(Me.cbbuku)
+        Me.Controls.Add(Me.dttanggal)
+        Me.Controls.Add(Me.cmbjenis)
+        Me.Controls.Add(Me.rbnon)
+        Me.Controls.Add(Me.rbmember)
+        Me.Controls.Add(Me.txtnama)
+        Me.Controls.Add(Me.txtlama)
+        Me.Controls.Add(Me.txtid)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgview, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtid As TextBox
+    Friend WithEvents txtlama As TextBox
+    Friend WithEvents txtnama As TextBox
+    Friend WithEvents rbmember As RadioButton
+    Friend WithEvents rbnon As RadioButton
+    Friend WithEvents cmbjenis As ComboBox
+    Friend WithEvents dttanggal As DateTimePicker
+    Friend WithEvents cbbuku As CheckBox
+    Friend WithEvents cbpensil As CheckBox
+    Friend WithEvents cbpena As CheckBox
+    Friend WithEvents bttambah As Button
+    Friend WithEvents btkeluar As Button
+    Friend WithEvents bthapus As Button
+    Friend WithEvents btsimpan As Button
+    Friend WithEvents dgview As DataGridView
+End Class
---
+++ Form1.vb
@@ -0,0 +1,173 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    Dim strkon As String = "server=localhost;uid=root;database=quiz_uas_2321022"
+    Dim kon As New MySqlConnection(strkon)
+    Dim perintah As New MySqlCommand
+    Dim cek As MySqlDataReader
+    Dim mda As New MySqlDataAdapter
+    Dim ds As New DataSet
+    Dim tipe, bonus As String
+
+    Sub tidakaktif()
+        txtid.Enabled = False
+        txtnama.Enabled = False
+        rbmember.Enabled = False
+        rbnon.Enabled = False
+        cmbjenis.Enabled = False
+        dttanggal.Enabled = False
+        txtlama.Enabled = False
+        cbbuku.Enabled = False
+        cbpena.Enabled = False
+        cbpensil.Enabled = False
+
+        txtid.BackColor = Color.Gray
+        txtnama.BackColor = Color.Gray
+        rbmember.BackColor = Color.Gray
+        rbnon.BackColor = Color.Gray
+        cmbjenis.BackColor = Color.Gray
+        dttanggal.BackColor = Color.Gray
+        txtlama.BackColor = Color.Gray
+        cbbuku.BackColor = Color.Gray
+        cbpena.BackColor = Color.Gray
+        cbpensil.BackColor = Color.Gray
+
+        btsimpan.Enabled = False
+        bthapus.Enabled = False
+        btkeluar.Enabled = False
+    End Sub
+
+    Sub aktif()
+        txtid.Enabled = True
+        txtnama.Enabled = True
+        rbmember.Enabled = True
+        rbnon.Enabled = True
+        cmbjenis.Enabled = True
+        dttanggal.Enabled = True
+        txtlama.Enabled = True
+        cbbuku.Enabled = True
+        cbpena.Enabled = True
+        cbpensil.Enabled = True
+
+        txtid.BackColor = Color.White
+        txtnama.BackColor = Color.White
+        rbmember.BackColor = Color.White
+        rbnon.BackColor = Color.White
+        cmbjenis.BackColor = Color.White
+        dttanggal.BackColor = Color.White
+        txtlama.BackColor = Color.White
+        cbbuku.BackColor = Color.White
+        cbpena.BackColor = Color.White
+        cbpensil.BackColor = Color.White
+
+        btsimpan.Enabled = True
+        bthapus.Enabled = True
+        btkeluar.Enabled = True
+    End Sub
+
+    Sub bersih()
+        txtid.Text = ""
+        txtnama.Text = ""
+        txtlama.Text = ""
+    End Sub
+
+    Sub tampil()
+        kon.Open()
+        perintah.Connection = kon
+        perintah.CommandType = CommandType.Text
+        perintah.CommandText = "select * from perpus"
+        mda.SelectCommand = perintah
+        ds.Tables.Clear()
+        mda.Fill(ds, "perpus")
+        dgview.DataSource = ds.Tables("perpus")
+        kon.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tidakaktif()
+        bersih()
+        tampil()
+    End Sub
+    Private Sub txtid_TextChanged(sender As Object, e As EventArgs) Handles txtid.TextChanged
+        kon.Open()
+        perintah.Connection = kon
+        perintah.CommandType = CommandType.Text
+        perintah.CommandText = "SELECT * from perpus where id='" & txtid.Text & "'"
+        cek = perintah.ExecuteReader
+        cek.Read()
+        If cek.HasRows Then
+            txtnama.Text = cek.Item("nama")
+            tipe = cek.Item("tipe")
+            If tipe = "member" Then
+                rbmember.Checked = True
+            Else
+                rbnon.Checked = True
+            End If
+            cmbjenis.Text = cek.Item("jenis")
+            dttanggal.Value = cek.Item("tanggal")
+            txtlama.Text = cek.Item("lama")
+            bonus = cek.Item("bonus")
+            If bonus = "buku" Then
+                cbbuku.Checked = True
+            ElseIf bonus = "pena" Then
+                cbpena.Checked = True
+            Else
+                cbpensil.Checked = True
+            End If
+        End If
+        kon.Close()
+    End Sub
+
+    Private Sub btsimpan_Click(sender As Object, e As EventArgs) Handles btsimpan.Click
+        If rbmember.Checked = True Then
+            tipe = "Laki-Laki"
+        Else
+            tipe = "Perempuan"
+        End If
+
+        If cbbuku.Checked = True Then
+            bonus = "buku"
+        ElseIf cbpena.Checked = True Then
+            bonus = "pena"
+        Else
+            bonus = "pensil"
+        End If
+        kon.Open()
+        perintah.Connection = kon
+        perintah.CommandType = CommandType.Text
+        perintah.CommandText = "insert into perpus values " &
+        " ('" & txtid.Text & "','" & txtnama.Text & "', " &
+        " '" & tipe & "','" & cmbjenis.Text & "', " &
+        " '" & Format(dttanggal.Value, "yyyy-MM-dd") & "','" & txtlama.Text &
+        "','" & bonus & "')"
+        perintah.ExecuteNonQuery()
+        kon.Close()
+        MsgBox("data berhasil disimpan", MsgBoxStyle.Information, "Pesan")
+        tampil()
+        bersih()
+        tidakaktif()
+        bttambah.Enabled = True
+        btkeluar.Enabled = True
+    End Sub
+
+    Private Sub bthapus_Click(sender As Object, e As EventArgs) Handles bthapus.Click
+        kon.Open()
+        perintah.Connection = kon
+        perintah.CommandType = CommandType.Text
+        perintah.CommandText = "delete from perpus where id='" & txtid.Text & "'"
+        perintah.ExecuteNonQuery()
+        kon.Close()
+        tampil()
+        bersih()
+        tidakaktif()
+    End Sub
+
+    Private Sub btkeluar_Click(sender As Object, e As EventArgs) Handles btkeluar.Click
+        End
+    End Sub
+
+    Private Sub bttambah_Click(sender As Object, e As EventArgs) Handles bttambah.Click
+        aktif()
+        txtid.Focus()
+        bttambah.Enabled = False
+    End Sub
+End Class
